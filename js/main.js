1173fd8d18ddd718f6eef2869fed2a7f3f92994d
Alexis <alexis@incogito.org>
2020-08-27 19:38:15 +0200

    - Added: Rendering the hotkey to image - Updated: CSS - Fixed: HTML formatting

--- js/main.js
+++ js/main.js
@@ -28,30 +28,58 @@ $('document').ready(function () {
         k90: 'Z',
     };
 
+    $('#save-btn').click(function () {
+      var e = document.getElementById("output-location");
+      var e_width = e.offsetWidth;
+      var e_height = e.offsetHeight;
+      var e_x_offset = window.scrollX + e.getBoundingClientRect().left;
+      var e_y_offset = window.scrollY + e.getBoundingClientRect().top;
 
+      html2canvas(e, {
+        scale: 1,
+        backgroundColor: null,
+        width: e_width,
+        height: e_height,
+        x: e_x_offset,
+        y: e_y_offset }).then(canvas => {
+        //document.body.appendChild(canvas)
+        	var base64image = canvas.toDataURL("image/png");
+          var win = window.open('', "_blank");
+          win.document.write('<img src="' + base64image + '"/>');
+          win.document.close();
+      });
+    });
 
     $('body').keydown(function (e) {
         const id = 'k'+e.keyCode;
         console.log('down = ' + keyMap[id]);
 
         var kbString = '';
+        var kbHtmlString = '';
 
         if (e.ctrlKey) {
             kbString += 'Ctrl + ';
+            kbHtmlString += '<kbd>Ctrl</kbd>-';
         }
 
         if(e.altKey){
             kbString += 'Alt + ';
+            kbHtmlString += '<kbd>Alt</kbd>-';
         }
 
         if (e.shiftKey) {
             kbString += 'Shift + ';
+            kbHtmlString += '<kbd>Shift</kbd>-';
         }
 
-        if (e.key != 'Control' && e.key != 'Shift' && e.key != 'Alt')
+        if (e.key != 'Control' && e.key != 'Shift' && e.key != 'Alt') {
             kbString += e.key;
+            kbHtmlString += '<kbd>' + e.key + '</kbd>';
+        }
 
         $('#kb-input').val(kbString);
+        $('#output-location').html(kbHtmlString);
+        
         e.preventDefault();
         e.stopPropagation();
     });
